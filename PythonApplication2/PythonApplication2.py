80587c641ab0b7f72af69013c18cc1bc75f44703
darioRus <dario_rus@hotmail.com>
2026-04-23 11:15:06 -0300

    añadida función de lectura de documento y un menú en desarrollo (siguiente función "borrar documento")

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,5 +1,6 @@
-#Datos para a usar#
+import opcode
 import os
+from tkinter import ARC
 
 game_name = "empty"
 game_ver = 0
@@ -8,20 +9,17 @@ game_notes = "empty"
 nombre_archivo_sav = "empty"
 game_dlcs = []
 yesNote = ""
-
 confirm_save = []
-
-#control de bucle#
 confirm_info = []
 
-##Gestión de archivos##
-#Creación de arhivo#
+create_read_delete = ""
+
 def generador_archivos(nombre_docuSave, nombreJuego, versionJuego, dlcsJuego, rutaSaves, notasDeJugador):
     try:
         #añade el .txt al final#
         if not nombre_docuSave.endswith(".txt"):
             nombre_docuSave += ".txt"
-
+           
         with open(nombre_docuSave, "w", encoding="utf-8") as f:
             f.write (f"Título: {nombreJuego}\n")
             f.write (f"Versión: {versionJuego}\n")
@@ -38,7 +36,6 @@ def generador_archivos(nombre_docuSave, nombreJuego, versionJuego, dlcsJuego, ru
 def limpiar_pantalla():
     print("\033[H\033[J", end="")
 
-#Comienzo del programa (FUNCIONES clave)#
 def solicitud_datos():
     #control de bucle#
     confirm_info = []
@@ -72,28 +69,53 @@ def solicitud_datos():
             break
     return game_name, game_ver, game_dlcs, game_saveFolder_location
 
-#Añadir anotación final#
 def nota_final():
     yesNote = input("\n¿Desea agregar notas adicionales? (ej: errores de mods, requisitos de hardware, etc.) --S/N--").upper()
     if yesNote == "S":
         game_notes = input("Notas: ")
         return game_notes
 
-#Pedir confirmación para generar el documento#
 def confirmar_generar_documento():
     confirm_save = input("Desea generar un archivo .txt con la información?:...(S - generar archivo/N - salir sin guardar) \n").upper()
     if confirm_save == "S":
         nombre_archivo_sav = input("Ingrese el título del proyecto: \n")
         generador_archivos(nombre_archivo_sav, game_name, game_ver, game_dlcs, game_saveFolder_location, game_notes)
     else:
-        print ("seliendo sin guardar. \n")
-    
+        print ("Saliendo sin guardar. \n")
+
+def lectura_documento():
+    archiveName = input("Ingrese el nombre del documento para ver:\n")
+    if not archiveName.endswith(".txt"):
+            archiveName += ".txt"
+
+    try:
+        with open (archiveName, "r", encoding="utf-8") as archivo:
+            contenidoLeido = archivo.read()
+            return contenidoLeido
+    except FileNotFoundError:
+        return "Error: documento no encontrado..."
+            
+
 #######Comienzo del programa#######
 if __name__ == "__main__":
-    game_name, game_ver, game_dlcs, game_saveFolder_location = solicitud_datos()
-    limpiar_pantalla()
-    game_notes = nota_final()
-    confirmar_generar_documento()
 
+    while create_read_delete != "salir":
+        create_read_delete = input("Desea crear / leer / borrar documento? (o salir:)\n").lower()
+        if create_read_delete == "crear":
+            #crear documento#
+            game_name, game_ver, game_dlcs, game_saveFolder_location = solicitud_datos()
+            limpiar_pantalla()
+            game_notes = nota_final()
+            confirmar_generar_documento()
+
+        elif create_read_delete == "leer":
+            limpiar_pantalla()
+            #Ver documento#
+            contenidoMostrar = lectura_documento()
+            print(contenidoMostrar)
+            input("\nPresiona Enter para volver al menú...")
+            
+        
+    
 
                                                                                                 
